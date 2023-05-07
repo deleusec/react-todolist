23ea692c8241a3e1cb694a9926d0ce6a35aee9d8
Corentin Deleuse <dlse.corentin@gmail.com>
2023-05-07 22:26:19 +0200

    add todo at list

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { TodoForm } from "./TodoForm";
+import { Todo } from "./Todo";
+import { v4 as uuidv4 } from "uuid";
+
+uuidv4();
 
 export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
   return (
-    <div>TodoWrapper</div>
-  )
-}
+    <div className="TodoWrapper">
+      <h1>Get Things Done !</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => {
+        return(
+          <Todo task={todo} key={index} />
+        )
+      })}
+    </div>
+  );
+};
